3b6196ce24b29a508d382883351637506f85b6b5
elangoram1998@gmail.com <elangoram1998@gmail.com>
2021-06-01 22:32:01 +0530

    video call 2

--- server/WebSocket.js
+++ server/WebSocket.js
@@ -1,5 +1,6 @@
 const Account = require('./models/account_collection');
 const ChatRoom = require('./models/charRoom_collection');
+const { addUser, getUser } = require('./users');
 
 class WebSocket {
 
@@ -25,6 +26,24 @@ class WebSocket {
             await chatRoom.save();
 
             global.io.to(roomId).emit('message', chatRoom.messages[messagesSize]);
+        });
+
+        socket.on('call', ({ id, myId, peerId }) => {
+            const getUserDetails = getUser(id);
+            if (getUserDetails) {
+                global.io.to(getUserDetails.socket).emit('getting-call', { myId, peerId });
+            }
+        });
+
+        socket.on('answer-call', ({ id, peerId }) => {
+            const getUserDetails = getUser(id);
+            if (getUserDetails) {
+                global.io.to(getUserDetails.socket).emit('call-picked', { peerId });
+            }
+        });
+
+        socket.on('disconnect-call', ({ id, peerId }) => {
+
         });
 
         socket.on('disconnect', () => {
